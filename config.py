7a257087eb9ab690e23c7c7817e312ad78875db6
anuj9719168804-cyber <anuj9719168804@gmail.com>
2026-01-03 22:28:44 +0530

    Update config.py

--- config.py
+++ config.py
@@ -1,7 +1,7 @@
-API_ID = 123456  # api id
-API_HASH = "ABC-DEF1234ghIkl-zyx57W2v1u123ew11"  # api hash
+API_ID = 32295602  # api id
+API_HASH = "406a1c848574cd54189041aa7e507984"  # api hash
 
-BOT_TOKEN = "123456:ABC-DEF1234ghIkl-zyx57W2v1u123ew11"  # bot token
+BOT_TOKEN = "8399022576:AAFe3QcCEso6rnyboCzpvSigC0DjY4jX_Iw"  # bot token
 
 
 # REDIS
@@ -15,7 +15,7 @@ COOKIE = ""
 ADMINS = [1317173146]
 
 
-BOT_USERNAME = "teraboxdown_bot"
+BOT_USERNAME = "@Teraboxanuj_bot"
 
 # Force user to join this channel. (make sure you have promoted the bot on this chat.)
 FORCE_LINK = "@RoldexVerse"
